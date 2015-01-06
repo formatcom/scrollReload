2d8b7d82d5578cb6383fe908ab12c1c39032d544
Vinicio Valbuena <formatcomvinicio@gmail.com>
2015-01-06 13:21:33 -0430

    se creo Paginador con scroll de ejemplo

--- assets/js/scrollController.js
+++ assets/js/scrollController.js
@@ -1,6 +1,16 @@
 $(function() { 
+
+	var paginador = 0;
+	
+	$('body').append(template());
+
 	$(this).scroll(function(){
 		log('Scroll-y: '+ $(this).scrollTop(), true);
 		log('Scroll-MaxY: '+ ($(document).height() - $(window).height()), true);
+
+		if ($(this).scrollTop() === ($(document).height() - $(window).height())){
+			paginador++;
+			$('body').append(template(paginador));
+		}
 	}); 
 });
---
+++ assets/js/template.js
@@ -0,0 +1,24 @@
+function template(pag){
+	html = '<div class="caja"></div>
+			<div class="caja"></div>
+			<div class="caja"></div>
+			<div class="caja"></div>
+			<div class="caja"></div>
+			<div class="caja"></div>
+			<div class="caja"></div>
+			<div class="caja"></div>
+			<div class="caja"></div>
+			<div class="caja"></div>
+			<div class="caja"></div>
+			<div class="caja"></div>
+			<div class="caja"></div>
+			<div class="caja"></div>
+			<div class="caja"></div>
+			<div class="caja"></div>
+			<div class="caja"></div>
+			<div class="caja"></div>
+	';
+	if (pag > 0)
+		alert('Pagina #'+ pag)
+	return html;
+}
